39ae484dbe816fb30d5722906d67c6c9b7e54f1d
leandroSiq1 <le_kts12@hotmail.com>
2021-06-11 10:43:26 -0300

    inserindo os dados pelo javascript e formatando amount

--- scripts/script.js
+++ scripts/script.js
@@ -14,33 +14,138 @@ const Modal = {
   }
 }
 
-// 2,30
-console.log(230 * 100);
+const Transaction = {
+  all: [
+    {
+      description: "Bolacha",
+      amount: 500,
+    },
+    {
+      description: "Guárana",
+      amount: -400,
+    },
+  ],
 
-const transactions = [
-  {
-    description: "Bolacha",
-    income: 23000,
-    expense: 10000,
+  table: document.querySelector("#table"), 
+
+  add(transaction) {
+    Transaction.all.push(transaction);
+    
+    App.reload();
   },
-];
+}
+
+const Utils = {
+  formatAmount(value) {
+    const amount = String(value).replace(",", "");
+
+    return Number(amount);
+  },
+
+  formatCurrency(value) {
+    value = Number(value) / 100;
+
+    value = value.toLocaleString("pr-BR", { style: "currency", currency: "BRL" })
+    
+    return value;
+  }
+}
+
+const DOM = {
+  addTransaction(transaction) {
+    const tr = document.createElement("tr");
+
+    tr.innerHTML = DOM.insertInnerHtml(transaction);
+    
+    Transaction.table.appendChild(tr);
+  },
+
+  insertInnerHtml(transaction) {
+    const CSSClass = transaction.amount < 0 ? "expense" : "income";
+    
+    const amount = Utils.formatCurrency(transaction.amount);
+
+    const HTML = `
+      <td class="description">${transaction.description}</td>
+      <td class="${CSSClass}">${amount}</td>
+      <td class="date">09/06/2021</td>
+      
+      <td>
+        <img src="assets/minus.svg" alt="Remover transação" />
+      </td>
+    `;
+
+    return HTML;
+  }
+}
 
 const Form = {
   description: document.querySelector("#description"),
-  product: document.querySelector("#product"),
-  customer: document.querySelector("#customer"),
+  amount: document.querySelector("#amount"),
   
-  clear() {
-    Form.description.value = "";
-    Form.product.value = "";
-    Form.customer.value = "";
+  submit(event) {
+    event.preventDefault();
+    
+    try {
+      Form.validateFields();
+
+      Transaction.add(Form.formatValues());
+      
+      Form.clear();
+
+      Modal.toggle();
+
+    } catch (error) {
+      console.log(error);
+    }
+
   },
 
-  getValue() { 
+  validateFields() {
+    const { description, amount } = Form.getValues();
+
+    if (description.trim() === "" || amount.trim() === "") {
+      throw new Error("Por favor, preencha todos os campos!");
+    } 
+  },
+
+  clear() {
+    Form.description.value = "";
+    Form.amount.value = "";
+  },
+
+  getValues() { 
     return {
       description: Form.description.value,
-      product: Form.product.value,
-      customer: Form.customer.value,
+      amount: Form.amount.value,
     }
+  },
+
+  formatValues() {
+    let { description, amount } = Form.getValues();
+
+    amount = Utils.formatAmount(amount);
+    
+    console.log(amount);
+    
+    return {
+      description,
+      amount,
+    };
   }
 }
+
+const  App = {
+  init() {
+    Transaction.all.forEach(transaction => {
+      DOM.addTransaction(transaction);
+    });
+  },
+
+  reload() {
+    Transaction.table.innerHTML = "";
+    App.init();
+  }
+}
+
+App.init();
